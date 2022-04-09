4bb7a868bc11a6d6307cb6b708e1bf084f64fdca
sumanta2 <sumantagorai.21@gmail.com>
2022-04-09 16:41:52 +0530

    4.a problem solved make app component

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include('travello.urls'), name='index'),
+    path('oldindex',views.index,name='indexOld'),
     path('rm', views.renderMe, name='rm'),
     path('add', views.add, name='add'),
 
