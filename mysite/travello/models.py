0616c6545ad09355ea9e6f4c23ec5f392d03ebb6
sumanta2 <sumantagorai.21@gmail.com>
2022-04-13 20:31:21 +0530

    6 populate value in html file using loop

--- mysite/travello/models.py
+++ mysite/travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+#from pyparsing import srange
 
 # Create your models here.
+
+class Destination:
+    id:int
+    name:str
+    img:str
+    price:int
--- mysite/travello/views.py
+++ mysite/travello/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 
 def index(request):
-    return render(request,'index.html')
+    dest1=Destination()
+    dest1.name='Mumbai'
+    dest1.price=700
+    dest1.img="destination_1.jpg"
+    dest1.desc="The city That Never Sleeps"
+
+    dest2=Destination()
+    dest2.name='Hydrabad'
+    dest2.price=650
+    dest2.img="destination_2.jpg"
+    dest2.desc="First Biriyani, Then Sherwani"
+
+    dest3=Destination()
+    dest3.name='Bengaluru'
+    dest3.price=750
+    dest3.img="destination_3.jpg"
+    dest3.desc="Beautiful City"   
+
+    dests= [dest1,dest2,dest3]
+    print(dests[0].name)
+    return render(request,'index.html',{'dests':dests})
