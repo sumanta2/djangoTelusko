8c5d4a863b2dd49e144c87bb7a233dea3afabdd0
sumanta2 <sumantagorai.21@gmail.com>
2022-04-17 08:27:02 +0530

    12 send message backend to fontend

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,13 +15,19 @@ def register(request):
         if password1==password2:
             if User.objects.filter(username=username).exists():
                 print("UserName Taken")
+                messages.info(request,"UserName Taken")
+                return redirect ('register')
             elif User.objects.filter(email=email).exists():
                 print("Email taken")
+                messages.info(request,"Email taken")
+                return redirect ('register')
             else:
                 user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)  # query(ORM) to save data in postgreSql database
                 user.save()
         else:
             print("Password are not matching")
+            messages.info(request,"Password are not matching")
+            return redirect ('register')
 
         return redirect('/')
     else:
--- mysite/travello/views.py
+++ mysite/travello/views.py
@@ -4,27 +4,6 @@ from .models import Destination
 
 
 def index(request):
-    # dest1=Destination()
-    # dest1.name='Mumbai'
-    # dest1.price=700
-    # dest1.img="destination_1.jpg"
-    # dest1.desc="The city That Never Sleeps"
-    # dest1.offer=False
-
-    # dest2=Destination()
-    # dest2.name='Hydrabad'
-    # dest2.price=650
-    # dest2.img="destination_2.jpg"
-    # dest2.desc="First Biriyani, Then Sherwani"
-    # dest2.offer=True
-
-    # dest3=Destination()
-    # dest3.name='Bengaluru'
-    # dest3.price=750
-    # dest3.img="destination_3.jpg"
-    # dest3.desc="Beautiful City"
-    # dest3.offer=False 
-    #dests= [dest1,dest2,dest3]
 
     dests= Destination.objects.all()   #it fetch all data present in postgreSql database's Destination table
 
